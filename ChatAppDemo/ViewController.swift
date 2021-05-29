e31222927df568fac17b7c8e5e4a99d7de7ac2ab
rinetrous <rinellie1228@icloud.com>
2021-05-29 14:44:27 +0900

    Initial Commit

---
+++ ChatAppDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatAppDemo
+//
+//  Created by 野田凜太郎 on 2021/05/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
